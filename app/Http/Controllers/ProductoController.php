04b9692692f9094871ce478371570353ab725351
f47iw4 <fgrhanny@gmail.com>
2025-11-23 19:51:24 +0100

    Add restricción de vista inactiva para el cliente

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,29 +6,34 @@ use App\Models\Producto;
 use Illuminate\Http\Request;
 
 // Controller para la parte pública de la aplicación,
-// no confundir con aProductoController (admin) para la parte privada (CRUD admin)
-// Este controlador maneja la parte PÚBLICA de la tienda.
-// Permite a los visitantes ver el listado de productos y sus detalles.
-// No requiere autenticación (a diferencia de Admin\aProductoController).
+// no confundir con AdminProductoController (admin) para la parte privada (CRUD admin)
+// Este controlador maneja la parte publica de la tienda.
+// Permite a los visitantes ver el listado de productos y sus detalles
+// No requiere autenticación (a diferencia de Admin\aProductoController)
 class ProductoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+    /**
+     * muestra al cliente sólo los productos activos
+     */
     public function index()
     {
-        $productos = Producto::where('activo', true)->get();
+        $productos = Producto::where('activo', true)
+                            ->orderBy('id', 'desc')
+                            ->paginate(25);
         return view('productos.index', compact('productos'));
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Producto $producto)
     {
+        // Verificar que el producto esté activo
         if (!$producto->activo) {
             abort(404);
         }
+    
         return view('productos.show', compact('producto'));
     }
 }
