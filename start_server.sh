2910bc670cb61b8d74974ad6cd66c79ae9cf39cf
f47iw4 <fgrhanny@gmail.com>
2025-12-07 18:02:33 +0100

    Add starter script

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export DB_CONNECTION=sqlite
+export DB_CONNECTION=mysql
+export DB_HOST=127.0.0.1
+export DB_PORT=3306
+export DB_DATABASE=mansus
+export DB_USERNAME=root
+export DB_PASSWORD=
 nohup php artisan serve > serve.log 2>&1 &
 echo $! > serve.pid
 echo "Server started with PID $(cat serve.pid)"
